c65b78443107cf4a93985602a9dad8928f10e47d
Brendan <shaefferbrendan@gmail.com>
2023-11-30 13:20:09 -0500

    edited styles for pages

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const multer = require("multer");
-// const upload = multer({ dest: "uploads/test" });
-
 const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 const profilesCtrl = require('../controllers/profiles')
